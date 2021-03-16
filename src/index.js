26ebd8b6e0d7836ccd275ddfa14c2fd5497db0f4
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-16 15:15:06 +0900

    未完了にリスト追加完了 未完了にリスト追加完了

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add_text").value;
+  document.getElementById("add_text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に書く要素を設定
+  div.appendChild(li);
+
+  //未完了のリストに追加
+  document.getElementById("inconplate_list").appendChild(div);
+
+  console.log(div);
+};
+
+document
+  .getElementById("add_button")
+  .addEventListener("click", () => onClickAdd());
